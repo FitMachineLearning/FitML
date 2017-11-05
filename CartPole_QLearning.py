a6f2d44133f88a28070b5f9ed6d57170e80c134c
Fit Machine Learning <33303615+FitMachineLearning@users.noreply.github.com>
2017-11-05 13:39:48 -0800

    Update CartPole_QLearning.py

--- CartPole_QLearning.py
+++ CartPole_QLearning.py
@@ -1,5 +1,9 @@
 '''
-Cartpole-v0 solution by the Author of the Fit Machine Learning Blog
+Cartpole-v0 solution by Michel Aka
+
+https://github.com/FitMachineLearning/FitML/
+
+Using DeepQ Learning
 
 '''
 import numpy as np
@@ -21,14 +25,14 @@ num_training_exmaples = 30
 timesteps = 1
 num_initial_observation = 60
 learning_rate = 0.001
-weigths_filename = "Cartpole-weights_DQN.h5"
+weigths_filename = "Cartpole-weights_DQN_Mem_1Loop.h5"
 
 b_discount = 0.95
 max_memory_len = 2000
 num_failures_for_retrain = 10
 starting_explore_prob = 0.05
-initial_training_epochs = 1000
-RL_training_eporcs = 1000
+initial_training_epochs = 2
+RL_training_eporcs = 2
 num_anticipation_steps = 6
 load_previous_weights = False
 observe_and_train = True
@@ -87,17 +91,61 @@ memoryY = np.zeros(shape=(1,1))
 print("dataX shape", dataX.shape)
 print("dataY shape", dataY.shape)
 
+
+
+def predictTotalRewards(qstate, action):
+    qs_a = np.concatenate((qstate,actions_1_hot[action]), axis=0)
+    predX = np.zeros(shape=(1,num_env_variables+num_env_actions))
+    predX[0] = qs_a
+
+    #print("trying to predict reward at qs_a", predX[0])
+    pred = model.predict(predX[0].reshape(1,predX.shape[1]))
+    remembered_total_reward = pred[0][0]
+    return remembered_total_reward
+
+
+
 if observe_and_train:
     #observe for 100 games
 
 
-    for game in range(50):
+    for game in range(500):
         gameX = np.zeros(shape=(1,num_env_variables+num_env_actions))
         gameY = np.zeros(shape=(1,1))
         #Get the Q state
         qs = env.reset()
         for step in range (1000):
-            a = env.action_space.sample()
+
+            if game < num_initial_observation:
+                #take a radmon action
+                a = env.action_space.sample()
+            else:
+                prob = np.random.rand(1)
+                explore_prob = starting_explore_prob-(starting_explore_prob/Learning_cycles)*game
+
+                #Chose between prediction and chance
+                if prob < explore_prob:
+                    #take a random action
+                    a=env.action_space.sample()
+                    #print("taking random action",a, "at total_steps" , total_steps)
+                    #print("prob ", prob, "explore_prob", explore_prob)
+
+                else:
+                    ##chose an action by estimating consequences of actions for the next num_anticipation_steps steps ahead
+                    #works best with looking 6 steps ahead
+                    #Also works best if you train the model more itterations
+                    utility_possible_actions = np.zeros(shape=(num_env_actions))
+
+                    utility_possible_actions[0] = predictTotalRewards(qs,0)
+                    utility_possible_actions[1] = predictTotalRewards(qs,1)
+
+
+                    #chose argmax action of estimated anticipated rewards
+                    #print("utility_possible_actions ",utility_possible_actions)
+                    #print("argmax of utitity", np.argmax(utility_possible_actions))
+                    a = np.argmax(utility_possible_actions)
+
+
 
             env.render()
             qs_a = np.concatenate((qs,actions_1_hot[a]), axis=0)
@@ -119,27 +167,10 @@ if observe_and_train:
             gameX = np.vstack((gameX,qs_a))
             gameY = np.vstack((gameY,np.array([r])))
 
-            memoryX = np.vstack((memoryX,qs_a))
-            memoryY = np.vstack((memoryY,np.array([r])))
-
-            #if memory is full remove first element
-            if np.alen(memoryX) >= max_memory_len:
-                print("memory full. mem len ", np.alen(memoryX))
-                memoryX = np.delete(memoryX, 0, axis=0)
-                memoryY = np.delete(memoryY, 0, axis=0)
-
-
-
-
-            #Update the states
-            qs=s
-
 
             if done :
-                #print("total steps ", total_steps)
-                #Set total score on all recorded gamestates
-                #print("total at the last step ", r)
-                #Set total score on all recorded gamestates
+                #GAME ENDED
+                #Calculate Q values from end to start of game
                 for i in range(0,gameY.shape[0]):
                     #print("Updating total_reward at game epoch ",(gameY.shape[0]-1) - i)
                     if i==0:
@@ -152,14 +183,31 @@ if observe_and_train:
                     if i==gameY.shape[0]-1:
                         print("Training Game #",game, " steps = ", step ,"last reward", r," finished with headscore ", gameY[(gameY.shape[0]-1)-i][0])
 
-
-                if dataX.shape[0] ==1:
-                    dataX = gameX
-                    dataY = gameY
+                if memoryX.shape[0] ==1:
+                    memoryX = gameX
+                    memoryY = gameY
                 else:
-                    #print("Adding Game X to data X",)
-                    dataX = np.concatenate((dataX,gameX), axis=0)
-                    dataY = np.concatenate((dataY,gameY), axis=0)
+                    #Add experience to memory
+                    memoryX = np.concatenate((memoryX,gameX),axis=0)
+                    memoryY = np.concatenate((memoryY,gameY),axis=0)
+
+                #if memory is full remove first element
+                if np.alen(memoryX) >= max_memory_len:
+                    print("memory full. mem len ", np.alen(memoryX))
+                    memoryX = np.delete(memoryX, 0, axis=0)
+                    memoryY = np.delete(memoryY, 0, axis=0)
+
+            #Update the states
+            qs=s
+
+            #Retrain every X failures after num_initial_observation
+            if done and game >= num_env_actions:
+                if game%10 == 0:
+                    print("Training  game# ", game,"momory size", memoryX.shape[0])
+                    model.fit(memoryX,memoryY, batch_size=32,epochs=initial_training_epochs,verbose=2)
+
+            if done:
+                #Game ended - Break
                 break
 
     print("Observation complete. - Begin LSTM training")
@@ -170,16 +218,13 @@ if observe_and_train:
     print(dataY)
 
 
-    #feedX = np.reshape(dataX, (dataX.shape[0], 1, dataX.shape[1] ))
-    #feedY = np.reshape(dataY, (dataY.shape[0], 1, dataY.shape[1] ))
-    feedX = dataX
-    feedY = dataY
+
 
 
     #The more epochs you train the model, the better is becomes at predicting future states
     #This in turn will improve the results of the Bellman equation and thus will lead us to
     # better decisions in our MDP process
-    model.fit(feedX,feedY, batch_size=32,epochs=initial_training_epochs,verbose=2)
+    #model.fit(feedX,feedY, batch_size=32,epochs=initial_training_epochs,verbose=2)
 
     #print("total_steps ", total_steps)
     #print("dataX ", dataX[0:10])
@@ -189,133 +234,6 @@ if observe_and_train:
 
 #dataX = np.random.random((1,9))
 
-def predictTotalRewards(qstate, action):
-    qs_a = np.concatenate((qstate,actions_1_hot[action]), axis=0)
-    predX = np.zeros(shape=(1,num_env_variables+num_env_actions))
-    predX[0] = qs_a
-
-    #print("trying to predict reward at qs_a", predX[0])
-    pred = model.predict(predX[0].reshape(1,predX.shape[1]))
-    remembered_total_reward = pred[0][0]
-    return remembered_total_reward
-
-
-#####
-#Play the game for X rounds using the Bellman with LSTM anticipation model
-
-explore_prob = starting_explore_prob
-failures = 1
-for game in range(Learning_cycles):
-
-    gameX = np.zeros(shape=(1,num_env_variables+num_env_actions))
-    gameY = np.zeros(shape=(1,1))
-
-    #Get the Q state
-    qs = env.reset()
-    #over the next 50 games reduce the probability of explore_prob
-    explore_prob = starting_explore_prob-(starting_explore_prob/Learning_cycles)*game
-    #print("- Episode", game, " explore_prob",explore_prob)
-    for step in range (18000):
-
-        prob = np.random.rand(1)
-
-        #Chose between prediction and chance
-        if prob < explore_prob:
-            #take a random action
-            a=env.action_space.sample()
-            #print("taking random action",a, "at total_steps" , total_steps)
-            #print("prob ", prob, "explore_prob", explore_prob)
-
-        else:
-            ##chose an action by estimating consequences of actions for the next num_anticipation_steps steps ahead
-            #works best with looking 6 steps ahead
-            #Also works best if you train the model more itterations
-            utility_possible_actions = np.zeros(shape=(num_env_actions))
-
-            utility_possible_actions[0] = predictTotalRewards(qs,0)
-            utility_possible_actions[1] = predictTotalRewards(qs,1)
-
-
-            #chose argmax action of estimated anticipated rewards
-            #print("utility_possible_actions ",utility_possible_actions)
-            #print("argmax of utitity", np.argmax(utility_possible_actions))
-            a = np.argmax(utility_possible_actions)
-
-        env.render()
-        #get the target state and reward
-        s,r,done,info = env.step(a)
-
-        if done and step <=196:
-            r = -1
-            failures+=1
-
-        qs=s
-
-        qs_a = np.concatenate((qs,actions_1_hot[a]), axis=0)
-        s_r = np.concatenate((s,np.array([r])), axis=0)
-
-
-        #record only the first x number of states
-        if step ==0:
-            gameX[0] = qs_a
-            gameY[0] = np.array([r])
-        else:
-            gameX = np.vstack((gameX,qs_a))
-            gameY = np.vstack((gameY,np.array([r])))
-
-        memoryX = np.vstack((memoryX,qs_a))
-        memoryY = np.vstack((memoryY,np.array([r])))
-
-        #if memory is full remove first element
-        if np.alen(memoryX) >= max_memory_len:
-            #print("memory full. mem len ", np.alen(memoryX))
-            memoryX = np.delete(memoryX, 0, axis=0)
-            memoryY = np.delete(memoryY, 0, axis=0)
-
-
-        #Update the states
-        qs=s
-
-        if done :
-
-
-            #print("total steps ", total_steps)
-            #Set total score on all recorded gamestates
-            for i in range(0,gameY.shape[0]):
-                #print("Updating total_reward at game epoch ",(gameY.shape[0]-1) - i)
-                if i==0:
-                    #print("*reward at the last step ",gameY[(gameY.shape[0]-1)-i][0])
-                    gameY[(gameY.shape[0]-1)-i][0] = gameY[(gameY.shape[0]-1)-i][0]
-                else:
-                    #print("local error before Bellman", gameY[(gameY.shape[0]-1)-i][0])
-                    gameY[(gameY.shape[0]-1)-i][0] = gameY[(gameY.shape[0]-1)-i][0]+b_discount*gameY[(gameY.shape[0]-1)-i+1][0]
-                    #print("reward at step",i,"away from the end is",gameY[(gameY.shape[0]-1)-i][0])
-
-                if i==gameY.shape[0]-1:
-                    print("Playing/practice Game #",game, " steps = ", step ," explore ", explore_prob," last reward ", r ," finished with headscore ", gameY[(gameY.shape[0]-1)-i][0])
-
-            if step >= 197:
-                print("***GAME WON")
-                print("***GAME WON")
-            else:
-                print("Game lost")
-
-            #print("gameX last 5",gameX[-5:])
-            #print("gameY last 5",gameY[-5:])
-
-
-
-
-            if Do_RL and failures%num_failures_for_retrain == 0:
-                print("Retraining the network with memoryX.len  ",np.alen(memoryX))
-                model.fit(memoryX,memoryY, batch_size=32,epochs=RL_training_eporcs,verbose=2)
-
-
-
-            break
-
-        if done:
-            break
 
 if save_weights:
     #Save model
