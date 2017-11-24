1c7a6c2e6f5952e9c1922f5604a3a8f148e6d467
Fit Machine Learning <33303615+FitMachineLearning@users.noreply.github.com>
2017-11-23 23:26:10 -0800

    Update BepedalWalker_A2C.py

--- ActorCritic/BepedalWalker_A2C.py
+++ ActorCritic/BepedalWalker_A2C.py
@@ -1,18 +1,22 @@
 '''
-BipedalWalker-v2 solution by Michel Aka
+BipedalWalker solution by Michel Aka
 https://github.com/FitMachineLearning/FitML/
 https://www.youtube.com/channel/UCi7_WxajoowBl4_9P0DhzzA/featured
-Experimental.
-A2C method.
-The model starts to show promise around 3000 itterations.
-I believe the technique doesn't scale well and needs a new approach. Maybe ACKTR.
-If someone is willing to try this and train it for a week to see what results we get, let me know
+
+Article about this solution
+https://github.com/FitMachineLearning/FitML/edit/master/ActorCritic/README.md
+Using Actor Critic
+Note that I prefe the terms Action Predictor Network and Q/Reward Predictor network better
+Update
+Cleaned up variables and more readable memory
+Improved hyper parameters for better performance
 '''
 import numpy as np
 import keras
 import gym
 import os
 import h5py
+import matplotlib.pyplot as plt
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
@@ -23,23 +27,24 @@ from keras import optimizers
 
 num_env_variables = 24
 num_env_actions = 4
-num_initial_observation = 0
-learning_rate =  0.003
-apLearning_rate = 0.001
-weigths_filename = "BpW-v3-weights.h5"
-apWeights_filename = "BpW-v3-AP-weights.h5"
+num_initial_observation = 50
+learning_rate =  0.01
+apLearning_rate = 0.008
+weigths_filename = "Waker-AC-v4-weights.h5"
+apWeights_filename = "Walker-AC-v4-weights-ap.h5"
 
 #range within wich the SmartCrossEntropy action parameters will deviate from
 #remembered optimal policy
 sce_range = 0.2
-b_discount = 0.95
-max_memory_len = 50000
-starting_explore_prob = 0.15
-training_epochs = 2
+b_discount = 0.98
+max_memory_len = 90000
+starting_explore_prob = 0.20
+training_epochs = 4
+mini_batch = 256
 load_previous_weights = True
 observe_and_train = True
 save_weights = True
-num_games_to_play = 9000
+num_games_to_play = 6000
 
 
 #One hot encoding array
@@ -60,15 +65,18 @@ dataY = np.random.random((5,1))
 apdataX = np.random.random(( 5,num_env_variables ))
 apdataY = np.random.random((5,num_env_actions))
 
+def custom_error(y_true, y_pred, Qsa):
+    cce=0.001*(y_true - y_pred)*Qsa
+    return cce
+
+
 #nitialize the Reward predictor model
-model = Sequential()
+Qmodel = Sequential()
 #model.add(Dense(num_env_variables+num_env_actions, activation='tanh', input_dim=dataX.shape[1]))
-model.add(Dense(4096, activation='tanh', input_dim=dataX.shape[1]))
-model.add(Dense(dataY.shape[1]))
-
+Qmodel.add(Dense(4096, activation='tanh', input_dim=dataX.shape[1]))
+Qmodel.add(Dense(dataY.shape[1]))
 opt = optimizers.adam(lr=learning_rate)
-
-model.compile(loss='mse', optimizer=opt, metrics=['accuracy'])
+Qmodel.compile(loss='mse', optimizer=opt, metrics=['accuracy'])
 
 
 #initialize the action predictor model
@@ -78,8 +86,8 @@ action_predictor_model.add(Dense(4096, activation='tanh', input_dim=apdataX.shap
 action_predictor_model.add(Dense(apdataY.shape[1]))
 
 opt2 = optimizers.adam(lr=apLearning_rate)
-
 action_predictor_model.compile(loss='mse', optimizer=opt2, metrics=['accuracy'])
+
 
 
 #load previous model weights if they exist
@@ -89,7 +97,7 @@ if load_previous_weights:
     print("filepath ", fn)
     if  os.path.isfile(fn):
         print("loading weights")
-        model.load_weights(weigths_filename)
+        Qmodel.load_weights(weigths_filename)
     else:
         print("File ",weigths_filename," does not exis. Retraining... ")
 
@@ -106,23 +114,12 @@ if load_previous_weights:
 
 
 
+memorySA = np.zeros(shape=(1,num_env_variables+num_env_actions))
+memoryS = np.zeros(shape=(1,num_env_variables))
+memoryA = np.zeros(shape=(1,1))
+memoryR = np.zeros(shape=(1,1))
 
-
-#Record first 500 in a sequence and add them to the training sequence
-total_steps = 0
-dataX = np.zeros(shape=(1,num_env_variables+num_env_actions))
-dataY = np.zeros(shape=(1,1))
-
-memoryX = np.zeros(shape=(1,num_env_variables+num_env_actions))
-memoryY = np.zeros(shape=(1,1))
-
-apmemoryX = np.zeros(shape=(1,num_env_variables))
-apmemoryY = np.zeros(shape=(1,num_env_actions))
-
-print("dataX shape", dataX.shape)
-print("dataY shape", dataY.shape)
-
-
+mstats = []
 
 def predictTotalRewards(qstate, action):
     qs_a = np.concatenate((qstate,action), axis=0)
@@ -130,7 +127,7 @@ def predictTotalRewards(qstate, action):
     predX[0] = qs_a
 
     #print("trying to predict reward at qs_a", predX[0])
-    pred = model.predict(predX[0].reshape(1,predX.shape[1]))
+    pred = Qmodel.predict(predX[0].reshape(1,predX.shape[1]))
     remembered_total_reward = pred[0][0]
     return remembered_total_reward
 
@@ -144,26 +141,15 @@ def GetRememberedOptimalPolicy(qstate):
     r_remembered_optimal_policy = pred[0]
     return r_remembered_optimal_policy
 
-def SmartCrossEntropy(current_optimal_policy):
-    sce = np.zeros(shape=(num_env_actions))
-    #print("current_optimal_policy", current_optimal_policy)
-    for i in range(num_env_actions):
-        sce[i] = current_optimal_policy[i] + sce_range * (np.random.rand(1)*2 - 1)
-        if sce[i] > 1:
-            sce[i] = 1.0
-        if sce[i] < -1:
-            sce[i] = -1
-    #print("current_optimal_policy", current_optimal_policy)
-    #print("sce", sce)
-    return sce
-
 
 if observe_and_train:
 
     #Play the game 500 times
     for game in range(num_games_to_play):
-        gameX = np.zeros(shape=(1,num_env_variables+num_env_actions))
-        gameY = np.zeros(shape=(1,1))
+        gameSA = np.zeros(shape=(1,num_env_variables+num_env_actions))
+        gameS = np.zeros(shape=(1,num_env_variables))
+        gameA = np.zeros(shape=(1,num_env_actions))
+        gameR = np.zeros(shape=(1,1))
         #Get the Q state
         qs = env.reset()
         #print("qs ", qs)
@@ -171,7 +157,7 @@ if observe_and_train:
             print("Observing game ", game)
         else:
             print("Learning & playing game ", game)
-        for step in range (4000):
+        for step in range (5000):
 
             if game < num_initial_observation:
                 #take a radmon action
@@ -183,32 +169,13 @@ if observe_and_train:
                 #Chose between prediction and chance
                 if prob < explore_prob:
                     #take a random action
-                    a=env.action_space.sample()
-                    #print("taking random action",a, "at total_steps" , total_steps)
-                    #print("prob ", prob, "explore_prob", explore_prob)
+                    a = env.action_space.sample()
 
                 else:
-                    ##chose an action by estimating function-estimator remembered consequences of all possible actions
-                    ## Bellman states that the best policy (i.e. action) is the one that maximizez expected rewards for future states
-                    ## to caculate rewards we compute the reward a this state t + the discounted (b_discount) reward at all possible state t+1
-                    ## all states t+1 are estimated by our function estimator (our Neural Network)
 
                     #Get Remembered optiomal policy
                     remembered_optimal_policy = GetRememberedOptimalPolicy(qs)
 
-                    '''
-                    #Generate a set of num_env_action*10
-                    possible_actions = np.zeros(shape=(num_env_actions*4,num_env_actions))
-                    utility_possible_actions = np.zeros(shape=(num_env_actions*4))
-                    for i in range(num_env_actions*4):
-                        possible_actions[i] = SmartCrossEntropy(remembered_optimal_policy)
-                        utility_possible_actions[i] = predictTotalRewards(qs,possible_actions[i])
-                    #print("utility_possible_actions", utility_possible_actions)
-                    #chose argmax action of estimated anticipated rewards
-                    #print("utility_possible_actions ",utility_possible_actions)
-                    #print("argmax of utitity", np.argmax(utility_possible_actions))
-                    best_sce_i = np.argmax(utility_possible_actions)
-                    '''
                     stock = np.zeros(9)
                     stockAction = np.zeros(shape=(9,num_env_actions))
                     for i in range(9):
@@ -217,73 +184,72 @@ if observe_and_train:
                     best_index = np.argmax(stock)
                     randaction = stockAction[best_index]
 
+                    #Compare R for SmartCrossEntropy action with remembered_optimal_policy and select the best
+                    #if predictTotalRewards(qs,remembered_optimal_policy) > utility_possible_actions[best_sce_i]:
                     if predictTotalRewards(qs,remembered_optimal_policy) > predictTotalRewards(qs,randaction):
                         a = remembered_optimal_policy
                         #print(" | selecting remembered_optimal_policy ",a)
                     else:
                         a = randaction
-
-                            #print(" - selecting generated optimal policy ",a)
-                    #a = remembered_optimal_policy
-
+                        #print(" - selecting generated optimal policy ",a)
 
 
             env.render()
             qs_a = np.concatenate((qs,a), axis=0)
 
-            #print("action",a," qs_a",qs_a)
             #get the target state and reward
             s,r,done,info = env.step(a)
             #record only the first x number of states
 
+
             if step ==0:
-                gameX[0] = qs_a
-                gameY[0] = np.array([r])
-                memoryX[0] = qs_a
-                memoryY[0] = np.array([r])
-                apmemoryX[0] = qs
-                apmemoryY[0] = a
+                gameSA[0] = qs_a
+                gameS[0] = qs
+                gameR[0] = np.array([r])
+                gameA[0] = np.array([r])
+            else:
+                gameSA= np.vstack((gameSA, qs_a))
+                gameS= np.vstack((gameS, qs))
+                gameR = np.vstack((gameR, np.array([r])))
+                gameA = np.vstack((gameA, np.array([a])))
 
-            gameX = np.vstack((gameX,qs_a))
-            gameY = np.vstack((gameY,np.array([r])))
-            apmemoryX = np.vstack((apmemoryX,qs))
-            apmemoryY = np.vstack((apmemoryY,a))
-
-            if step > 3500:
+            if step > 797:
                 done = True
-                #print("Too many steps. Game ended at step", step)
 
             if done :
-                #GAME ENDED
                 #Calculate Q values from end to start of game
-                for i in range(0,gameY.shape[0]):
+                mstats.append(step)
+                for i in range(0,gameR.shape[0]):
                     #print("Updating total_reward at game epoch ",(gameY.shape[0]-1) - i)
                     if i==0:
                         #print("reward at the last step ",gameY[(gameY.shape[0]-1)-i][0])
-                        gameY[(gameY.shape[0]-1)-i][0] = gameY[(gameY.shape[0]-1)-i][0]
+                        gameR[(gameR.shape[0]-1)-i][0] = gameR[(gameR.shape[0]-1)-i][0]
                     else:
                         #print("local error before Bellman", gameY[(gameY.shape[0]-1)-i][0],"Next error ", gameY[(gameY.shape[0]-1)-i+1][0])
-                        gameY[(gameY.shape[0]-1)-i][0] = gameY[(gameY.shape[0]-1)-i][0]+b_discount*gameY[(gameY.shape[0]-1)-i+1][0]
+                        gameR[(gameR.shape[0]-1)-i][0] = gameR[(gameR.shape[0]-1)-i][0]+b_discount*gameR[(gameR.shape[0]-1)-i+1][0]
                         #print("reward at step",i,"away from the end is",gameY[(gameY.shape[0]-1)-i][0])
-                    if i==gameY.shape[0]-1:
-                        print("Training Game #",game, " steps = ", step ,"last reward", r," finished with headscore ", gameY[(gameY.shape[0]-1)-i][0])
+                    if i==gameR.shape[0]-1:
+                        print("Training Game #",game, "memory ",memoryR.shape[0], " steps = ", step ,"last reward", r," finished with headscore ", gameR[(gameR.shape[0]-1)-i][0])
 
-                if memoryX.shape[0] ==1:
-                    memoryX = gameX
-                    memoryY = gameY
+                if memoryR.shape[0] ==1:
+                    memorySA = gameSA
+                    memoryR = gameR
+                    memoryA = gameA
+                    memoryS = gameS
                 else:
                     #Add experience to memory
-                    memoryX = np.concatenate((memoryX,gameX),axis=0)
-                    memoryY = np.concatenate((memoryY,gameY),axis=0)
+                    memorySA = np.concatenate((memorySA,gameSA),axis=0)
+                    memoryS = np.concatenate((memoryS,gameS),axis=0)
+                    memoryR = np.concatenate((memoryR,gameR),axis=0)
+                    memoryA = np.concatenate((memoryA,gameA),axis=0)
+
 
                 #if memory is full remove first element
-                if np.alen(memoryX) >= max_memory_len:
-                    memoryX = memoryX[gameX.shape[0]:]
-                    memoryY = memoryY[gameX.shape[0]:]
-
-                if np.alen(apmemoryX) >= max_memory_len:
-                    apmemoryX = apmemoryX[gameX.shape[0]:]
-                    apmemoryY = apmemoryY[gameX.shape[0]:]
+                if np.alen(memorySA) >= max_memory_len:
+                    memorySA = memorySA[gameR.shape[0]:]
+                    memoryR = memoryR[gameR.shape[0]:]
+                    memoryA = memoryA[gameR.shape[0]:]
+                    memoryS = memoryS[gameR.shape[0]:]
 
 
             #Update the states
@@ -292,36 +258,36 @@ if observe_and_train:
 
             #Retrain every X failures after num_initial_observation
             if done and game >= num_initial_observation:
-                if game%8 == 0 and game>20:
-                    print("Training  game# ", game,"momory size", memoryX.shape[0])
+                if game%5 == 0 :
+                    print("Training  game# ", game,"momory size", memorySA.shape[0])
 
                     #training Reward predictor model
-                    model.fit(memoryX,memoryY, batch_size=256,epochs=training_epochs,verbose=2)
+                    Qmodel.fit(memorySA,memoryR, batch_size=mini_batch,epochs=training_epochs,verbose=0)
 
                     #training action predictor model
-                    action_predictor_model.fit(apmemoryX,apmemoryY, batch_size=256, epochs=training_epochs,verbose=2)
+                    action_predictor_model.fit(memoryS,memoryA, batch_size=mini_batch, epochs=training_epochs,verbose=0)
 
             if done and game >= num_initial_observation:
-                if save_weights and game%10 == 0 and game>20:
+                if save_weights and game%20 == 0 :
                     #Save model
                     print("Saving weights")
-                    model.save_weights(weigths_filename)
+                    Qmodel.save_weights(weigths_filename)
                     action_predictor_model.save_weights(apWeights_filename)
 
             if done:
-                print("Game ended with reward ", r," steps ", step, "mem len", memoryX.shape[0],"apmem", apmemoryX.shape[0])
-                if r < 0:
-                    print("Game ",game," ended with - negative reward ")
-                if r >= 0 and r <99:
-                    print("Game ",game," ended with positive reward ")
-                if r > 50:
+                '''
+                #Game won  conditions
+                if step > 197:
                     print("Game ", game," WON *** " )
+                else:
+                    print("Game ",game," ended with positive reward ")
                 #Game ended - Break
+                '''
                 break
 
 
-
-
+plt.plot(mstats)
+plt.show()
 
 if save_weights:
     #Save model
