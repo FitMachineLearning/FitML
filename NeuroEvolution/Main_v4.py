3f317eaa73436c3a5fa01ea3c79c244a28594fc5
Fit Machine Learning <33303615+FitMachineLearning@users.noreply.github.com>
2018-04-24 12:09:17 -0700

    Update Main_v4.py

--- NeuroEvolution/Main_v4.py
+++ NeuroEvolution/Main_v4.py
@@ -1,3 +1,10 @@
+'''
+Neuro Evolution Algorithm by Michel Aka author of FitML github blog and repository
+https://github.com/FitMachineLearning/FitML/
+See the agents in action at
+https://www.youtube.com/channel/UCi7_WxajoowBl4_9P0DhzzA/featured
+'''
+
 import numpy as np
 import keras
 import gym
