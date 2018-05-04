b4129f2dbee21ce86442d426fd7c0802a86699ae
Fit Machine Learning <33303615+FitMachineLearning@users.noreply.github.com>
2018-05-04 15:37:47 -0700

    Update _MainAlgo_v4.2.py

--- DeepDeterministicSeletiveMemory/_MainAlgo_v4.2.py
+++ DeepDeterministicSeletiveMemory/_MainAlgo_v4.2.py
@@ -39,19 +39,23 @@ PLAY_GAME = False #Set to True if you want to agent to play without training
 uses_critic = True
 uses_parameter_noising = True
 
+ENVIRONMENT_NAME = "LunarLanderContinuous-v2"
 num_env_variables = 8
 num_env_actions = 2
+
+
 num_initial_observation = 0
-learning_rate =  0.002
-apLearning_rate = 0.001
+learning_rate =  0.0002
+apLearning_rate = 0.0001
+
+MUTATION_PROB = 0.4
+
 littl_sigma = 0.00006
 big_sigma = 0.01
-upper_delta = 0.035
-lower_delta = 0.01
+upper_delta = 0.0375
+lower_delta = 0.015
 #gaussSigma = 0.01
-MUTATION_PROB = 0.1
-ENVIRONMENT_NAME = "LunarLanderContinuous-v2"
-version_name = ENVIRONMENT_NAME + "ker_v10"
+version_name = ENVIRONMENT_NAME + "ker_v11"
 weigths_filename = version_name+"-weights.h5"
 apWeights_filename = version_name+"-weights-ap.h5"
 
@@ -60,12 +64,12 @@ apWeights_filename = version_name+"-weights-ap.h5"
 #remembered optimal policy
 sce_range = 0.2
 b_discount = 0.98
-max_memory_len = 50000
-experience_replay_size = 25000
+max_memory_len = 100000
+experience_replay_size = 2500
 random_every_n = 50
 num_retries = 60
 starting_explore_prob = 0.005
-training_epochs = 3
+training_epochs = 2
 mini_batch = 512*4
 load_previous_weights = False
 observe_and_train = True
@@ -78,7 +82,10 @@ random_num_games_to_play = num_games_to_play/3
 USE_GAUSSIAN_NOISE = True
 CLIP_ACTION = True
 HAS_REWARD_SCALLING = False
-max_steps = 1490
+USE_ADAPTIVE_NOISE = True
+HAS_EARLY_TERMINATION_REWARD = False
+EARLY_TERMINATION_REWARD = -5
+max_steps = 995
 
 
 
@@ -123,15 +130,15 @@ def custom_error(y_true, y_pred, Qsa):
 #nitialize the Reward predictor model
 Qmodel = Sequential()
 #model.add(Dense(num_env_variables+num_env_actions, activation='tanh', input_dim=dataX.shape[1]))
-Qmodel.add(Dense(512, activation='relu', input_dim=dataX.shape[1]))
+Qmodel.add(Dense(10024, activation='relu', input_dim=dataX.shape[1]))
 #Qmodel.add(Dropout(0.5))
-Qmodel.add(Dense(256, activation='relu'))
+#Qmodel.add(Dense(512, activation='relu'))
 #Qmodel.add(Dropout(0.5))
-Qmodel.add(Dense(4, activation='relu'))
+#Qmodel.add(Dense(4, activation='relu'))
 #Qmodel.add(Dropout(0.5))
 Qmodel.add(Dense(dataY.shape[1]))
-#opt = optimizers.adam(lr=learning_rate)
-opt = optimizers.Adadelta()
+opt = optimizers.rmsprop(lr=learning_rate)
+#opt = optimizers.Adadelta()
 
 Qmodel.compile(loss='mse', optimizer=opt, metrics=['accuracy'])
 
@@ -139,15 +146,15 @@ Qmodel.compile(loss='mse', optimizer=opt, metrics=['accuracy'])
 #initialize the action predictor model
 action_predictor_model = Sequential()
 #model.add(Dense(num_env_variables+num_env_actions, activation='tanh', input_dim=dataX.shape[1]))
-action_predictor_model.add(Dense(100, activation='relu', input_dim=apdataX.shape[1]))
+action_predictor_model.add(Dense(10024, activation='relu', input_dim=apdataX.shape[1]))
 #action_predictor_model.add(Dropout(0.5))
-action_predictor_model.add(Dense(50, activation='relu'))
+#action_predictor_model.add(Dense(512, activation='relu'))
 #action_predictor_model.add(Dropout(0.5))
-action_predictor_model.add(Dense(2, activation='relu'))
+#action_predictor_model.add(Dense(2, activation='relu'))
 #action_predictor_model.add(Dropout(0.5))
 action_predictor_model.add(Dense(apdataY.shape[1]))
-#opt2 = optimizers.adam(lr=apLearning_rate)
-opt2 = optimizers.Adadelta()
+opt2 = optimizers.rmsprop(lr=apLearning_rate)
+#opt2 = optimizers.Adadelta()
 
 action_predictor_model.compile(loss='mse', optimizer=opt2, metrics=['accuracy'])
 
@@ -155,11 +162,11 @@ action_predictor_model.compile(loss='mse', optimizer=opt2, metrics=['accuracy'])
 #initialize the action predictor model
 noisy_model = Sequential()
 #model.add(Dense(num_env_variables+num_env_actions, activation='tanh', input_dim=dataX.shape[1]))
-noisy_model.add(Dense(100, activation='relu', input_dim=apdataX.shape[1]))
+noisy_model.add(Dense(10024, activation='relu', input_dim=apdataX.shape[1]))
 #noisy_model.add(Dropout(0.5))
-noisy_model.add(Dense(50, activation='relu'))
+#noisy_model.add(Dense(512, activation='relu'))
 #noisy_model.add(Dropout(0.5))
-noisy_model.add(Dense(2, activation='relu'))
+#noisy_model.add(Dense(2, activation='relu'))
 #noisy_model.add(Dropout(0.5))
 noisy_model.add(Dense(apdataY.shape[1]))
 opt3 = optimizers.Adadelta()
@@ -222,6 +229,7 @@ mGames = []
 mAverageScores = []
 mSteps = []
 mAP_Counts = 0
+oldAPCount = 0
 num_add_mem = 0
 mAPPicks = []
 
@@ -283,7 +291,7 @@ def add_noise_to_model(targetModel,largeNoise = False):
             if USE_GAUSSIAN_NOISE:
                 w[0] = add_gaussian_noise(w[0],big_sigma,largeNoise)
             else:
-                w[0] = add_noise(w[0],largeNoise)
+                w[0] = add_noise_simple(w[0],largeNoise)
 
         targetModel.layers[k].set_weights(w)
     return targetModel
@@ -426,7 +434,7 @@ def pr_actor_experience_replay(memSA,memR,memS,memA,memW,num_epochs=1):
                 tW[i]=0.15
             #if (tR[i]>pr and tS[i]>gameAverage):
             #    tW[i]=0.25
-            if (tR[i]>pr + d*0.5 and tS[i]>gameAverage):
+            if (tR[i]>pr + d*0.0 ):
                 tW[i]=1
             #if (tR[i]>pr+d*0.005 and tR[i]>game_max) :
             #    tW[i] = 1
@@ -504,7 +512,7 @@ def actor_experience_replay(memSA,memR,memS,memA,memW,num_epochs=1):
         if t%89==1:
             print("%8d were better After removing first element"%np.alen(tX_train), "Upper_cut",memoryR.mean()+stdDev,"gameStdDev",memoryW.mean()+gameStdDev)
         if np.alen(tX_train)>0:
-            action_predictor_model.fit(tX_train,tY_train, batch_size=mini_batch, nb_epoch=1,verbose=0)
+            action_predictor_model.fit(tX_train,tY_train, batch_size=mini_batch, nb_epoch=2,verbose=0)
 
 
 
@@ -555,16 +563,17 @@ def add_controlled_noise(targetModel,big_sigma,largeNoise = False):
         delta = c.mean()
 
         deltaCount+=1
-        if delta > upper_delta:
-            big_sigma = big_sigma *0.9
-            #print("Delta",delta," out of bound adjusting big_sigma", big_sigma)
+        if USE_ADAPTIVE_NOISE:
+            if delta > upper_delta:
+                big_sigma = big_sigma *0.9
+                #print("Delta",delta," out of bound adjusting big_sigma", big_sigma)
 
-        if delta < lower_delta:
-            big_sigma = big_sigma *1.1
-            #print("Delta",delta," out of bound adjusting big_sigma", big_sigma)
-        #if delta > 3 or delta <0.01:
-        #    print("Delta",delta," out of bound adjusting big_sigma", big_sigma, "to",1/delta)
-        #    big_sigma = 1 / delta
+            if delta < lower_delta:
+                big_sigma = big_sigma *1.1
+                #print("Delta",delta," out of bound adjusting big_sigma", big_sigma)
+            #if delta > 3 or delta <0.01:
+            #    print("Delta",delta," out of bound adjusting big_sigma", big_sigma, "to",1/delta)
+            #    big_sigma = 1 / delta
     print("Tried x time ", deltaCount,"delta =", delta,"big_sigma ",big_sigma)
     return targetModel,big_sigma
 
@@ -630,11 +639,12 @@ for game in range(num_games_to_play):
                 #print("Using Actor")
                 if is_noisy_game and uses_parameter_noising:
                     remembered_optimal_policy = GetRememberedOptimalPolicyFromNoisyModel(noisy_model,qs)
+                    #mAP_Counts = oldAPCount
                 else:
                     remembered_optimal_policy = GetRememberedOptimalPolicy(qs)
                 a = remembered_optimal_policy
 
-                if uses_critic:
+                if uses_critic :
                     #print("Using critric")
                     stock = np.zeros(num_retries)
                     stockAction = np.zeros(shape=(num_retries,num_env_actions))
@@ -670,8 +680,9 @@ for game in range(num_games_to_play):
         s,r,done,info = env.step(a)
         #record only the first x number of states
 
-        #if done and step<max_steps-3:
-        #    r = -100
+        if HAS_EARLY_TERMINATION_REWARD:
+            if done and step<max_steps-3:
+                r = EARLY_TERMINATION_REWARD
         if True or HAS_REWARD_SCALLING:
             r=r/200 #reward scalling to from [-1,1] to [-100,100]
 
@@ -800,7 +811,7 @@ for game in range(num_games_to_play):
                     tR = tR[train_A,:]
                     tSA = tSA    [train_A,:]
                     #print("Training Critic n elements =", np.alen(tR))
-                    Qmodel.fit(tSA,tR, batch_size=mini_batch, nb_epoch=1,verbose=0)
+                    Qmodel.fit(tSA,tR, batch_size=mini_batch, nb_epoch=2,verbose=0)
             if game > 3 and game %5 ==-1 and uses_parameter_noising:
                 print("Training noisy_actor")
                 train_noisy_actor()
@@ -826,6 +837,7 @@ for game in range(num_games_to_play):
 
 
         if done:
+            oldAPCount = mAP_Counts
             if gameR.mean() >0:
                 num_positive_avg_games += 1
             if game%1==0:
@@ -839,7 +851,10 @@ for game in range(num_games_to_play):
                 mGames.append(game)
                 mSteps.append(step/1000*100)
                 mAPPicks.append(mAP_Counts/step*100)
-                mAverageScores.append(max(memoryR.mean(), -150))
+
+                mAverageScores.append(max(memoryR.mean()*200, -150))
+
+
                 bar_chart = pygal.HorizontalLine()
                 bar_chart.x_labels = map(str, mGames)                                            # Then create a bar graph object
                 bar_chart.add('Average score', mAverageScores)  # Add some values
