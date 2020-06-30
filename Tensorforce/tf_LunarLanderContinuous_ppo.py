25a78b6f0dea5c432ac2078b0675477f702a9ac2
Fit Machine Learning <33303615+FitMachineLearning@users.noreply.github.com>
2020-06-30 09:25:23 -0700

    Add files via upload

--- Tensorforce/tf_LunarLanderContinuous_ppo.py
+++ Tensorforce/tf_LunarLanderContinuous_ppo.py
@@ -7,28 +7,11 @@ from tensorforce.environments import OpenAIGym
 #     environment='gym', level='CartPole', max_episode_timesteps=500
 # )
 
-# Network as list of layers
-network_spec = [
-    # dict(type='dense', size=32, activation='tanh'),
-    dict(type='dense', size=128, activation='tanh')
-]
 
 # environment = OpenAIGym('CartPole-v0', visualize=True, max_episode_steps=500)
-environment = OpenAIGym('LunarLanderContinuous-v2', visualize=True, max_episode_steps=500)
+environment = OpenAIGym('LunarLanderContinuous-v2', visualize=False, max_episode_steps=500)
 # environment = OpenAIGym('BipedalWalker-v3', visualize=False, max_episode_steps=500)
 
-
-# Instantiate a Tensorforce agent
-# agent = Agent.create(
-#     agent='tensorforce',
-#     environment=environment,  # alternatively: states, actions, (max_episode_timesteps)
-#     memory=10000,
-#     update=dict(unit='timesteps', batch_size=64),
-#     optimizer=dict(type='adam', learning_rate=3e-4),
-#     policy=dict(network='auto'),
-#     objective='policy_gradient',
-#     reward_estimation=dict(horizon=20)
-# )
 
 agent = Agent.create(
     agent='ppo', environment=environment, batch_size=10,
@@ -39,16 +22,6 @@ agent = Agent.create(
     learning_rate=1e-3
 
 )
-
-# # Instantiate a Tensorforce agent
-# agent = PPOAgent(
-#     environment=environment,
-#     network=[
-#         dict(type='dense', size=64),
-#         dict(type='dense', size=64)
-#     ],
-#     learning_rate=1e-3
-# )
 
 
 running_score = 0.0
@@ -70,6 +43,11 @@ for i_epoch in range(50000):
     if i_epoch%5==0:
         print("Game ", i_epoch, "       game score %.2f"%game_score,"       running score %.2f"%running_score)
 
+    if i_epoch%10==0 and i_epoch>20:
+        agent.save()
+    if running_score >= 250:
+        agent.save()
+        break()
 
 agent.close()
 environment.close()
