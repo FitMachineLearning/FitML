a60f49fce1799ca4b11b48307441325b6272719a
Fit Machine Learning <33303615+FitMachineLearning@users.noreply.github.com>
2020-07-11 20:20:33 -0700

    Target score, custom policy,

--- Stable_baselines3/ppo_load.py
+++ Stable_baselines3/ppo_load.py
@@ -1,19 +1,23 @@
 import gym
+import pybullet, pybullet_envs
+import torch as th
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 
-
 # Create environment
-env = gym.make('LunarLanderContinuous-v2')
+# env = gym.make('LunarLanderContinuous-v2')
 
+env = gym.make('BipedalWalker-v3')
+env.render(mode="human")
+
+policy_kwargs = dict(activation_fn=th.nn.LeakyReLU, net_arch=[512, 512])
 # Instantiate the agent
-model = PPO('MlpPolicy', env, verbose=2)
-
+model = PPO('MlpPolicy', env,learning_rate=0.0003,policy_kwargs=policy_kwargs, verbose=1)
 
 del model  # delete trained model to demonstrate loading
 # Load the trained agent
-model = PPO.load("ppo_lunar")
+model = PPO.load("ppo_Ant")
 
 # # Evaluate the agent
 # mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
@@ -23,12 +27,14 @@ obs = env.reset()
 for i in range(100):
     dones = False
     game_score = 0
+    steps = 0
     while not dones:
         action, _states = model.predict(obs, deterministic=True)
         obs, rewards, dones, info = env.step(action)
         # import ipdb;ipdb.set_trace()
         game_score+=rewards
+        steps+=1
         env.render()
-    print("game ", i , " game score %.3f"%game_score)
+    print("game ", i ," steps   ",steps, " game score %.3f"%game_score)
     obs = env.reset()
     # break
--- Stable_baselines3/ppo_main2.py
+++ Stable_baselines3/ppo_main2.py
@@ -1,28 +1,38 @@
 import gym
+import pybullet, pybullet_envs
+import torch as th
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 
 
 # Create environment
-env = gym.make('LunarLanderContinuous-v2')
+# env = gym.make('LunarLanderContinuous-v2')
 
+env = gym.make('BipedalWalker-v3')
+# env.render(mode="human")
+
+policy_kwargs = dict(activation_fn=th.nn.LeakyReLU, net_arch=[512, 512])
 # Instantiate the agent
-model = PPO('MlpPolicy', env, verbose=1)
+model = PPO('MlpPolicy', env,learning_rate=0.0003,policy_kwargs=policy_kwargs, verbose=1)
 # Train the agent
-for i in range(10):
+for i in range(8000):
     print("Training itteration ",i)
     model.learn(total_timesteps=10000)
     # Save the agent
-    model.save("ppo_lunar")
-
+    model.save("ppo_Ant")
+    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=5)
+    print("mean_reward ", mean_reward)
+    if mean_reward >= 270:
+        print("***Agent Trained with average reward ", mean_reward)
+        break
 
 del model  # delete trained model to demonstrate loading
 # Load the trained agent
-model = DQN.load("ppo_lunar")
+# model = PPO.load("ppo_Ant")
 
 # Evaluate the agent
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+# mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 
 # Enjoy trained agent
 # obs = env.reset()
